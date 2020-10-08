2874a77d990bd49b5adc24362493fd0a6c2432b8
luka <DoBok444>
2020-10-08 23:24:14 +0200

    Did 3 things
    1. added a project with test for messing with inheritance
    2. Added test for testing the effect of a static field
    3. Added test for abstract methods

---
+++ inheritance/Inheritance.Tests/AnimalTests.cs
@@ -0,0 +1,53 @@
+using System;
+using Xunit;
+using Inheritance;
+
+namespace Inheritance.Tests
+{
+    public class AnimalTests : IDisposable
+    {
+        [Fact]
+        public void Creating_2_Animals_returns_population_of_two()
+        {
+            //Given
+            Animal dog = new Dog();
+            Animal cat = new Cat();
+
+            //Then
+            Assert.Equal(2, dog.GetCurrentPopulation());
+            Assert.Equal(2, cat.GetCurrentPopulation());
+        }
+
+
+        [Fact]
+        public void Given_dog_produces_sound_of_Woof()
+        {
+            //Given
+            var expected = "Woof";
+            var dog = new Dog();
+
+            //Then
+            Assert.Equal(expected, dog.Sound());
+        }
+
+        [Fact]
+        public void Given_cat_produces_sound_of_Miaw()
+        {
+            //Given
+            var expected = "Miaw";
+            var cat = new Cat();
+
+            //Then
+            Assert.Equal(expected, cat.Sound());
+        }
+
+        // Needed because of the static variable
+        // This is an example of horrible code, which is intentional as the
+        // meaning of it is to better understand static
+        public void Dispose()
+        {
+            var resetDog = new Dog();
+            resetDog.ResetCurrentPopulation();
+        }
+    }
+}
---
+++ inheritance/Inheritance/BadInheritance.cs
@@ -0,0 +1,54 @@
+using System;
+
+namespace Inheritance
+{
+    public abstract class Animal
+    {
+        public static int population { get; set; }
+        public string name;
+
+        public Animal()
+        {
+            population++;
+        }
+
+        // TODO Why do we need a get method, once the field is static?
+        public int GetCurrentPopulation()
+        {
+            return population;
+        }
+
+        public void ResetCurrentPopulation()
+        {
+            population = 0;
+        }
+
+        public abstract string Sound();
+
+        public override string ToString()
+        {
+            return $"I'm a: {name}";
+        }
+    }
+
+    public class Dog : Animal
+    {
+        public Dog()
+        {
+            name = "Dog";
+        }
+
+        public override string Sound()
+        {
+            return "Woof";
+        }
+    }
+
+    public class Cat : Animal
+    {
+        public override string Sound()
+        {
+            return "Miaw";
+        }
+    }
+}
